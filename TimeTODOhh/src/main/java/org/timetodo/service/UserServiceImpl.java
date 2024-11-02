23a564e8e3e7e280f39e489e8337e5f0836b2ea6
SEUIL <sone91961@gamil.com>
2024-11-02 20:46:00 +0900

    로그인 기능 구현, development_process.md 파일에 현재까지의 백엔드 개발과정 명시

--- TimeTODOhh/src/main/java/org/timetodo/service/UserServiceImpl.java
+++ TimeTODOhh/src/main/java/org/timetodo/service/UserServiceImpl.java
@@ -5,14 +5,24 @@ import org.springframework.stereotype.Service;
 import org.timetodo.entity.UserEntity;
 import org.timetodo.repository.UserRepository;
 
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+
     public void saveUser(UserEntity userEntity) {
+
         userRepository.save(userEntity);
+    }
+
+    @Override
+    public UserEntity authenticateUser(String username, String password) {
+        return userRepository.findByUsername(username)
+                .filter(user -> user.getPassword().equals(password)) // 평문 비교
+                .orElse(null);  // 사용자 정보가 맞지 않으면 null 반환
     }
 
     // 추가 기능들: 사용자 검색, 삭제 등
