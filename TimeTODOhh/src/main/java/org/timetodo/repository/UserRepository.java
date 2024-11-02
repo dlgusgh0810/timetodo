14f09e5381902a0bcbc581fbfe615d0775fa2560
DESKTOP-Q59EFEU\dlgus <dlgusgh0810@naver.com>
2024-11-02 14:09:42 +0900

    회원가입 수정

--- TimeTODOhh/src/main/java/org/timetodo/repository/UserRepository.java
+++ TimeTODOhh/src/main/java/org/timetodo/repository/UserRepository.java
@@ -2,9 +2,15 @@ package org.timetodo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.timetodo.model.User;
+import org.timetodo.entity.UserEntity;
+
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     // 필요한 경우 커스텀 쿼리 메소드 추가 가능
+
+    // 사용자 이름으로 검색
+    Optional<org.timetodo.entity.UserEntity> findByUsername(String username);
+
 }
